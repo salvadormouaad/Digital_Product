7b740a4f084e9a920623a6063a198bff19d3cd3a
mouad <osfourmouad32@gmail.com>
2025-12-14 13:58:05 +0100

    14 - 12 error2

--- src/Lists/Reviews.jsx
+++ src/Lists/Reviews.jsx
@@ -1,35 +1,35 @@
 const Active_Reviews = [
     {   
         id:1,
-        image:'src/assets/review1.png',
+        image:'assets/review1.png',
     },
     {
         id:2,
-        image:'src/assets/review2.png',
+        image:'assets/review2.png',
     },
     {
         id:3,
-        image:'src/assets/review3.png',
+        image:'assets/review3.png',
     },
     {
         id:4,
-        image:'src/assets/review4.png',
+        image:'assets/review4.png',
     },
     {
         id:5,
-        image:'src/assets/review5.png',
+        image:'assets/review5.png',
     },
     {
         id:6,
-        image:'src/assets/review6.png',
+        image:'assets/review6.png',
     },
     {
         id:7,
-        image:'src/assets/review7.png',
+        image:'assets/review7.png',
     },
     {
         id:8,
-        image:'src/assets/review8.png',
+        image:'assets/review8.png',
     },
     
 
